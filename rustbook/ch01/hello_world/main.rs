21317811cbf0840e2252a7170c7bb56e27aed20a
Kevin Chalmers <drkevinchalmers@gmai.com>
2021-12-18 18:39:12 +0000

    Added first program

---
+++ rustbook/ch01/hello_world/main.rs
@@ -0,0 +1,7 @@
+// This is the main function - entry point into the program
+fn main() {
+    // println! is a macro used to print to the command line
+    println!("Hello, world!");
+}
+
+// To compile, use rustc main.rs
