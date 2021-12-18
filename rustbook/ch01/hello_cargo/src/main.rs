f2ad6cf3db22dcdc1c3b8c5a6aa26ceb6254306c
Kevin Chalmers <drkevinchalmers@gmai.com>
2021-12-18 18:48:04 +0000

    First cargo build added.

---
+++ rustbook/ch01/hello_cargo/src/main.rs
@@ -0,0 +1,9 @@
+// Created used cargo new hello_cargo
+// This also created the Cargo.toml file
+// Use cargo build to build
+// Use cargo build --release to build for release
+// Use cargo run to run
+// Use cargo check to check if code will compile
+fn main() {
+    println!("Hello, world!");
+}
